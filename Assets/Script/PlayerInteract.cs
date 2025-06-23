c6fe15feb26c78ebd07f4010e058263edf5c4f7e
LuisPaprika <67010359@kmitl.ac.th>
2025-06-23 13:37:31 +0700

    Change how player handle interact action

--- Assets/Script/PlayerInteract.cs
+++ Assets/Script/PlayerInteract.cs
@@ -7,27 +7,19 @@ public class PlayerInteract : MonoBehaviour
     void Awake()
     {
         inputActions = new InputSystem_Actions();
+        inputActions.Enable();
     }
 
-    void OnEnable()
-    {
-        inputActions.Player.Enable();
-        inputActions.Player.Interact.performed += OnPlayerInteract;
-    }
-
-    void OnDisable()
-    {
-        inputActions.Player.Disable();
-        inputActions.Player.Interact.performed -= OnPlayerInteract;
-    }
-
-    private void OnPlayerInteract(InputAction.CallbackContext ctx)
+    void Update()
     {
         if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 100f))
         {
-            if (hit.collider.TryGetComponent<IInteractable>(out IInteractable interactObj))
+            if (inputActions.Player.Interact.WasPerformedThisFrame())
             {
-                interactObj.Interact();
+                if (hit.collider.TryGetComponent<IInteractable>(out IInteractable interactObj))
+                {
+                    interactObj.Interact();
+                }
             }
         }
     }
