39b83e236065be31c5ef48385a36eb169d24a7e7
LuisPaprika <67010359@kmitl.ac.th>
2025-06-23 23:25:41 +0700

    Implement drawer, Create lake scene, set player postion and add fishing pole to player camera

--- Assets/Script/DrawerInteract.cs
+++ Assets/Script/DrawerInteract.cs
@@ -2,8 +2,18 @@ using UnityEngine;
 
 public class DrawerInteract : InteractableBase
 {
+    [SerializeField] Animator DrawerAnimator;
+    private bool interacted = false;
     public override void Interact()
     {
-        Debug.Log("Drawer Interacted");
+        interacted = !interacted;
+        if (interacted)
+        {
+            DrawerAnimator.SetTrigger("OpenDrawer");
+        }
+        else
+        {
+            DrawerAnimator.SetTrigger("CloseDrawer");
+        }
     }
 }
